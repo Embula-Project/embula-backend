45d89643414c2203cb402682ffa77130142d1b89
Dewmin Deniyegedara <dewminkasmitha30@gmail.com>
2025-08-01 00:36:48 +0530

    Initial commit - Spring Boot backend with MongoDB

---
+++ src/main/java/com/embula/embula_backend/EmbulaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.embula.embula_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmbulaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmbulaBackendApplication.class, args);
+	}
+
+}
