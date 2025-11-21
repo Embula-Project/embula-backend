ff81c229f69775571e7f08b96c8cd3c1b702a603
TharushiDSilva <desilva.tharushiw@gmail.com>
2025-11-21 23:28:10 +0530

    feat(controller): add DiscountController endpoint and define the eaxt logic between serive and repository

---
+++ src/main/java/com/embula/embula_backend/controller/DiscountController.java
@@ -0,0 +1,37 @@
+package com.embula.embula_backend.controller;
+
+
+import com.embula.embula_backend.entity.Discount;
+import com.embula.embula_backend.services.DiscountService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/discounts")
+@RequiredArgsConstructor
+public class DiscountController {
+
+    private final DiscountService discountService;
+
+    @GetMapping
+    public List<Discount> getAllDiscounts() {
+        return discountService.getAllDiscounts();
+    }
+
+    @GetMapping("/active")
+    public List<Discount> getActiveDiscounts() {
+        return discountService.getActiveDiscounts();
+    }
+
+    @GetMapping("/today")
+    public List<Discount> getTodayDiscounts() {
+        return discountService.getTodayDiscounts();
+    }
+
+    @PostMapping
+    public Discount addDiscount(@RequestBody Discount discount) {
+        return discountService.saveDiscount(discount);
+    }
+}
---
+++ src/main/java/com/embula/embula_backend/services/impl/DiscountServiceImpl.java
@@ -0,0 +1,39 @@
+package com.embula.embula_backend.services.impl;
+
+
+import com.embula.embula_backend.entity.Discount;
+import com.embula.embula_backend.repository.DiscountRepository;
+import com.embula.embula_backend.services.DiscountService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class DiscountServiceImpl implements DiscountService {
+
+    private final DiscountRepository discountRepository;
+
+    @Override
+    public List<Discount> getAllDiscounts() {
+        return discountRepository.findAll();
+    }
+
+    @Override
+    public List<Discount> getActiveDiscounts() {
+        return discountRepository.findByIsActiveTrue();
+    }
+
+    @Override
+    public List<Discount> getTodayDiscounts() {
+        LocalDate today = LocalDate.now();
+        return discountRepository.findByValidFromLessThanEqualAndValidToGreaterThanEqual(today, today);
+    }
+
+    @Override
+    public Discount saveDiscount(Discount discount) {
+        return discountRepository.save(discount);
+    }
+}
