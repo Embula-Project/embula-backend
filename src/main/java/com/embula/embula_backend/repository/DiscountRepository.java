45d564fe6ac4f218082061908de7f9b58f12d062
TharushiDSilva <desilva.tharushiw@gmail.com>
2025-11-21 23:19:49 +0530

    Add repository for methods to create db queries using springJPA and  add service for discounts to define the business logic such as to check the active status of discount, dicounts mathcing fot this day etc.

--- src/main/java/com/embula/embula_backend/repository/DiscountRepository.java
+++ src/main/java/com/embula/embula_backend/repository/DiscountRepository.java
@@ -1,4 +1,16 @@
 package com.embula.embula_backend.repository;
 
-public class DiscountRepository {
+import com.embula.embula_backend.entity.Discount;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public interface DiscountRepository extends JpaRepository<Discount, Long> {
+
+    List<Discount> findByIsActiveTrue();
+
+    List<Discount> findByValidFromLessThanEqualAndValidToGreaterThanEqual(
+            LocalDate today1, LocalDate today2
+    );
 }
--- src/main/java/com/embula/embula_backend/services/DiscountService.java
+++ src/main/java/com/embula/embula_backend/services/DiscountService.java
@@ -1,4 +1,14 @@
 package com.embula.embula_backend.services;
 
+
+import com.embula.embula_backend.entity.Discount;
+
+import java.util.List;
+
 public interface DiscountService {
+    List<Discount> getAllDiscounts();
+    List<Discount> getActiveDiscounts();
+    List<Discount> getTodayDiscounts();
+    Discount saveDiscount(Discount discount);
 }
+
